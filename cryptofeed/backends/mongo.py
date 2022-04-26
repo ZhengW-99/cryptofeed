0415143ea00bd1ba47ef645760936e2c65f49e4e
ZhengW-99 <99930036@qq.com>
2022-04-27 05:35:45 +0800

    update

--- cryptofeed/backends/mongo.py
+++ cryptofeed/backends/mongo.py
@@ -6,6 +6,7 @@ associated with this software.
 '''
 from collections import defaultdict
 from datetime import timezone, datetime as dt
+import time
 
 import bson
 import motor.motor_asyncio
@@ -22,6 +23,10 @@ class MongoCallback(BackendQueue):
         self.running = True
         self.just = {}
 
+        self.gap = 10
+        self.R = {}
+        self.mp = {'bid':[], 'ask':[]}
+
     async def writer(self):
         conn = motor.motor_asyncio.AsyncIOMotorClient(self.uri)
         db = conn[self.db]
@@ -29,6 +34,7 @@ class MongoCallback(BackendQueue):
             async with self.read_queue() as updates:
                 for index in range(len(updates)):
                     data = updates[index]
+                    # print(data)
                     await db[data['symbol'].replace("-","_") + '_' + data['_type']].update_one(data['loc'], {'$set': data}, upsert = True)
         
 class TradeMongo(MongoCallback, BackendCallback):
@@ -64,7 +70,6 @@ class BookMongo(MongoCallback, BackendBookCallback):
         super().__init__(*args, **kwargs)
     
     async def write(self, data: dict):
-        self.gap=3600
         insert = {'_id': data['timestamp'], 
                   'receipt_timestamp': data['receipt_timestamp'],
                   'symbol': data['symbol'],
@@ -73,9 +78,12 @@ class BookMongo(MongoCallback, BackendBookCallback):
                  }
         if not self.just.__contains__(data['symbol']):
             self.just[data['symbol']] = 0
-        
-        if (data['timestamp']//self.gap-self.just[data['symbol']]>0.5):
+        if not self.R.__contains__(data['symbol']):
+            self.R[data['symbol']] = int(time.time()/self.gap)*self.gap
+
+        if (data['timestamp'] >= self.R[data['symbol']]):
         # save snapshot
+            self.R[data['symbol']] += self.gap
             insert1=insert.copy()
             price = {'ask': [], 'bid': []}
             size = {'ask': [], 'bid': []}
@@ -86,24 +94,51 @@ class BookMongo(MongoCallback, BackendBookCallback):
                     size[side]=[0 if i is None else float(i)  for i in list(data['book'][side].values())]
                 insert1[name[side][0]] = price[side] 
                 insert1[name[side][1]] = size[side]
-            self.just[data['symbol']]=(data['timestamp']+ 0.0001)//self.gap
-            insert1['_type'] = 'book'
+            
+
+            self.mp['bid'] = {}
+            self.mp['ask'] = {}
+
+            for i in range(len(price['bid'])):
+                self.mp['bid'][price['bid'][i]] = size['bid'][i]
+            
+            for i in range(len(price['ask'])):
+                self.mp['ask'][price['ask'][i]] = size['ask'][i]
+
+            self.just[data['symbol']]=(data['timestamp']+ 0.0001)
+            insert1['_type'] = 'Book'
             if self.multiprocess:
                 self.queue[1].send(insert1)
             else:
                 await self.queue.put(insert1)   
                 
         if data['delta'] is not None:
+            
             price = {'ask': [], 'bid': []}
             size = {'ask': [], 'bid': []}
-            name = {'ask':['ask_price', 'ask_size'], 'bid': ['bid_price', 'bid_size']}
+            delta = {'ask': [], 'bid': []}
+            name = {'ask':['ask_price', 'ask_size', 'ask_delta'], 'bid': ['bid_price', 'bid_size', 'bid_delta']}
             for side in ['bid', 'ask']:
                 for i in range(0, len(data['delta'][side])):
-                    price[side].append(data['delta'][side][i][0])
-                    size[side].append(data['delta'][side][i][1])
-                insert[name[side][0]] = price[side] 
+                    price[side].append(float(data['delta'][side][i][0]))
+                    size[side].append(float(data['delta'][side][i][1]))
+
+                    # next - pre
+                    if self.mp[side].__contains__(float(data['delta'][side][i][0])):
+                        delta[side].append(
+                            round(float(data['delta'][side][i][1]) -
+                            self.mp[side][float(data['delta'][side][i][0])],5)
+                        )
+                    else:
+                        delta[side].append(
+                            round(float(data['delta'][side][i][1]),5)
+                        )
+
+                insert[name[side][0]] = price[side]
                 insert[name[side][1]] = size[side]
-            insert['_type'] = 'delta'
+                insert[name[side][2]] = delta[side]
+
+            insert['_type'] = 'Delta'
             if self.multiprocess:
                 self.queue[1].send(insert)
             else:
